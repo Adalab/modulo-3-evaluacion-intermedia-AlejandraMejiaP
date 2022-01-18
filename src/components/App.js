f38b4ba2e40c730bdf1e5301e4230b8458355ea6
AlejandraMejiaP <alejandra.mejia.3c@gmail.com>
2022-01-18 10:19:55 +0100

    filter by name

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,34 @@ import { useEffect, useState } from "react";
 import "../styles/App.scss";
 
 function App() {
+  const [adalabersData, setAdalabersData] = useState([]);
   const [newStudents, setNewStudents] = useState([]);
+  const [search, setSearch] = useState("");
   const [data, setData] = useState({
     name: "",
     speciality: "",
     tutor: "",
   });
+  useEffect(() => {
+    fetch(
+      "https://beta.adalab.es/pw-recursos/apis/adalabers-v1/promo-patata.json"
+    )
+      .then((response) => response.json())
+      .then((dataFromApi) => {
+        setAdalabersData(dataFromApi.results);
+      });
+  }, []);
 
+  const filterData = adalabersData.filter((result)=>
+  result.name.toLowerCase().includes(search.toLowerCase())
+  )
+  const htmlAdalabers = filterData.map((adalaber) => (
+    <tr key={adalaber.id}>
+      <th>{adalaber.name}</th>
+      <th>{adalaber.counselor}</th>
+      <th>{adalaber.speciality}</th>
+    </tr>
+  ));
   const handleNewStudent = (ev) => {
     const newData = ev.currentTarget.name;
     setData({ ...data, [newData]: ev.currentTarget.value });
@@ -26,6 +47,11 @@ function App() {
       <th>{student.speciality}</th>
     </tr>
   ));
+
+  const handleChangeSearch = (ev) => {
+    setSearch(ev.currentTarget.value);
+  }
+
   const handleSubmitForm = (ev) => {
     ev.preventDefault();
   };
@@ -37,26 +63,44 @@ function App() {
       </header>
       <main>
         <section>
+          <form action=""  className="form__filter" onSubmit={handleSubmitForm}>
+          <label htmlFor="filter" className="form__filter--label">Filtar por tutora: 
+              <select
+                name="filter"
+                id="filter"
+                className="form__filter--selector">
+                <option value="todas">Todas</option>
+                <option value="dayana">Dayana</option>
+                <option value="yanelis">Yanelis</option>
+                <option value="ivan">Iván</option>
+              </select>
+            </label>
+            <label htmlFor="filter"> filtrar por nombre:</label>
+          <input type="search" 
+          autoComplete="off"
+          name="filter"
+          className="form__filter--input"
+          value={search} 
+          onChange={handleChangeSearch}
+          />
+          </form>
           <table className="table">
             <thead className="table__thead">
               <tr>
                 <th>Nombre</th>
                 <th>Tutora</th>
                 <th>Especialidad</th>
+                
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th>Pepa</th>
-                <th>Yanelis</th>
-                <th>IA</th>
-              </tr>
+              {htmlAdalabers}
               {htmlNewStudent}
             </tbody>
           </table>
         </section>
         <section>
-          <h3 className="section__title">Añadir un nuevo club</h3>
+          <h3 className="section__title">Añadir nueva adalaber</h3>
           <form action="" className="form" onSubmit={handleSubmitForm}>
             <label
               htmlFor="newStudent"
